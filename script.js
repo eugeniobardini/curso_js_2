bc6c0940e591d66fedc662a8bc1f7427950af9b6
John <Sapo@gmail.com>
2025-09-03 18:17:34 -0500

    ejercicio demo clase 8

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let a = prompt("ingrese el primer numero");
 a = Number(a);
 let b = prompt("ingrese el segundo numero");
@@ -19,3 +20,31 @@ if (operacion === 1) {
     resultado = "dato no valido";
 }
 alert("el resultado es " + resultado);
+*/
+
+opcion = Number(prompt("ingrese una opcion para ejecutar 1.suma de dos numeros enteros 2.Cuenta regresiva mayor a 10 3.Ingreso de datos personales"))
+switch(opcion){
+    case 1:
+        a = Number(prompt("ingrese el primer numero entero"))
+        b = Number(prompt("ingrese el segundo numero entero"))
+        console.log(a)
+        if(a!=NaN && b!=NaN){alert("Se ingreso una letra no un numero")
+        }else if(a > 0 && b > 0){console.log(`la suma de ${a} + ${b} = ${a+b}`)
+            }else{alert("el numero debe ser positivo")};
+    break;
+    case 2:
+        cuenta = Number(prompt("ingrese un numero mayor a 10"))  
+      if(cuenta > 10){
+            for(i = cuenta; i >= 0; i--){
+                console.log(i)
+            }
+        }else{alert("numero menor a 10 no es valido")};
+    break;
+    case 3:
+        nombre = prompt("porfavor ingrese su nombre")
+        edad = prompt("porfavor ingrese su edad")
+        console.log(`Hola ${nombre} tu edad es ${edad}`);
+    break;
+    default:
+        alert("opcion no valida")
+}
