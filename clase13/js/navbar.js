ef0df8efe9210217a504099b15df452f4784ec1c
Orlando Tapias <orpand21@gmail.com>
2025-10-20 18:18:21 -0500

    retirando_archivos

--- clase13/js/navbar.js
+++
@@ -1,9 +0,0 @@
-const titulos = ["Index", "Mujer", "Hombre", "Accesorios"];
-let menu = [];
-
-for (let titulo of titulos) {
-    menu.push(`<li><a href="/${titulo.toLowerCase()}.html">${titulo}</a></li>`);
-}
-
-const header = document.querySelector('header nav ul');
-header.innerHTML = menu.join("");
